d894dd898becc6284c13496d3632160d8a6085df
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-04-01 16:30:55 +0900

    [Add] plane pages

---
+++ components/CreateName.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{ this.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    CreateName() {
+      this.name = 'test'
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
